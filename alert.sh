5cdf84b51eaac959bd1a2df192b0c1db6aab4208
Jerry <juanino@gmail.com>
2015-07-24 21:30:16 -0400

    added samples

---
+++ alert.sh
@@ -0,0 +1,3 @@
+echo "sending alert for water_s1................"
+banner alert water
+# command to send mail or alert goes here
---
+++ clear.sh
@@ -0,0 +1,3 @@
+echo "sending clear for water_s1"
+banner clear water
+# enter command action to do on clear here, i.e. sms or other
