38e23f00db5a341631b56b34c5e3fbe7ec51494a
Jerry Uanino <juanino@gmail.com>
2017-04-18 22:06:17 -0400

    modified closet to use sns. sample sns sender provided

---
+++ example-sendsns.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+# this is is meant to be called from water.py or water2.py
+# change the 999999999999 to your proper ARN for the topic to publish to
+
+msg=$2
+
+/usr/local/bin/aws sns publish --topic-arn "arn:aws:sns:us-east-1:999999999999:waterpi" --message "${msg}"
