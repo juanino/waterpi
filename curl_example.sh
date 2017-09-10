76f4e5c1fb098a41c4b457b2f6e009b2d35fde7e
Jerry Uanino <juanino@gmail.com>
2017-09-10 11:11:21 -0400

    removed old curl example and moved to a gist

--- curl_example.sh
+++
@@ -1,7 +0,0 @@
-# post json to dweet.io
-curl    \
-	--header "Content-Type:application/json"    \
-	--header "Accept: application/json"  \
-	--request POST    \
-	--data '{"temp_last":"2:3:aa","temp":"20"}' \
-	 https://dweet.io/dweet/for/jgu1
