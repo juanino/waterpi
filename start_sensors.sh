545e98a4025ffc260819b32ae67962772d4a7158
Jerry Uanino <juanino@gmail.com>
2017-09-10 10:33:18 -0400

    cant guarantee eth0 is always the name, widen the grep net

--- start_sensors.sh
+++ start_sensors.sh
@@ -9,5 +9,5 @@ SENSOR_HOME=/home/pi/waterpi
 PATH=$SENSOR_HOME:/usr/local/bin/:$PATH
 cd $SENSOR_HOME
 echo "Raspberry pi rebooted" | $SENSOR_HOME/send_slack.py
-echo `ip addr  show eth0 |grep inet` |  $SENSOR_HOME/send_slack.py
+echo `ip addr  show  |grep inet` |  $SENSOR_HOME/send_slack.py
 screen -dmS sensors -c screenSensorRc
