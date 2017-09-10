eb232e6af014b411f323c39efb31db5582f044cc
Jerry Uanino <juanino@gmail.com>
2017-09-10 09:57:57 -0400

    added comments on aws requirements for sns

--- example-sendsns.sh
+++ example-sendsns.sh
@@ -3,6 +3,8 @@
 # this is is meant to be called from water.py or water2.py
 # change the 999999999999 to your proper ARN for the topic to publish to
 
+# requires "pip install awscli" then "aws configure"
+
 msg=$2
 
 /usr/local/bin/aws sns publish --topic-arn "arn:aws:sns:us-east-1:999999999999:waterpi" --message "${msg}"
