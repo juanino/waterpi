81af105b8229ddfeb2cbbb3b36e3414e2eb580d3
Jerry Uanino <juanino@gmail.com>
2017-09-10 10:45:56 -0400

    cleaned up ip discovery with hostname -I that i have never seen

--- start_sensors.sh
+++ start_sensors.sh
@@ -8,6 +8,15 @@
 SENSOR_HOME=/home/pi/waterpi
 PATH=$SENSOR_HOME:/usr/local/bin/:$PATH
 cd $SENSOR_HOME
-echo "Raspberry pi rebooted" | $SENSOR_HOME/send_slack.py
-echo `ip addr  show  |grep inet` |  $SENSOR_HOME/send_slack.py
+#echo "Raspberry pi rebooted" | $SENSOR_HOME/send_slack.py
+
+# Print the IP address
+_IP=$(hostname -I) || true
+if [ "$_IP" ]; then
+	  printf "My IP address is %s\n" "$_IP"
+fi
+
+#echo `ip addr  show  |grep inet` |  $SENSOR_HOME/send_slack.py
+echo [`hostname`] start_sensors.sh says,  "My ip address is $_IP" | $SENSOR_HOME/send_slack.py
+
 screen -dmS sensors -c screenSensorRc
